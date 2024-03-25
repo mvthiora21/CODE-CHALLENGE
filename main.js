dc0fa3f826640775cf7e1181f7d451f4424eded7
mvthiora21 <162286158+mvthiora21@users.noreply.github.com>
2024-03-25 09:45:13 +0300

    main.js

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const salary=100000
+const salary=100000//acquires input values
 const benefits=10000
 function calculateNetsalary(){
+    //calculates the tax
     let tax = Math.floor((salary+benefits) *0.45);
+    //calculates NHIF deductions
     let NHIFDeductions=Math.floor(salary * 0.08);
+    //calculates NSSF  deductions
     let NSSFDeductions=Math.floor(salary+benefits)
+    //calculates netsalary
     let netsalary=grosssalary - (tax+NHIFDeductions+NSSFDeductions);
     return{
         grosssalary,
@@ -13,7 +17,7 @@ function calculateNetsalary(){
         netsalary,
     }
 }
-let output = calculateNetsalary(salary,benefits)
+let output = calculateNetsalary(salary,benefits)//outputs netsalary
 console.log('grosssalary:', output.grosssalary)
 console.log('NHIFDeductions:',output.deductions)
 console.log('tax:', output.tax)
